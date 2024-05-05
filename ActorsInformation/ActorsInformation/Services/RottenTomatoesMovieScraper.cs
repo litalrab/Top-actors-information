5eeb5021fc1a511b8c1283e18e24b1b4679ccb66
litalrab <38513468+litalrab@users.noreply.github.com>
2024-05-05 18:45:10 +0300

    Add files via upload

---
+++ ActorsInformation/ActorsInformation/Services/RottenTomatoesMovieScraper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActorsInformation.Services
+{
+    public class RottenTomatoesMovieScraper : MoviesScraperBase
+    {
+        private static readonly string ROTTENTOMATOES_URL = "";
+        private static readonly string SOURCE = "RottenTomatoes";
+
+        private static readonly string MOVIES_EL_ID = "";
+        private static readonly string NAME_EL_ID = "";
+        private static readonly string RANK_EL_ID = "";
+        private static readonly string TYPE_EL_ID = "";
+        private static readonly string DETAILS_EL_ID = "";
+
+        public RottenTomatoesMovieScraper() : base(ROTTENTOMATOES_URL, MOVIES_EL_ID, NAME_EL_ID, RANK_EL_ID, TYPE_EL_ID, DETAILS_EL_ID, SOURCE)
+        {
+        }
+    }
+}
